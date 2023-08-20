63dd542d4914efc0ad1686fc09fdc7f9fd3158c0
Maca <hesss.marcel@gmail.com>
2023-08-20 15:06:01 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+import Links from './components/Links'
+import './index.css'
+
+function App() {
+
+  return (
+    <main>
+      <Links />
+      <section id='s1' className='section-1'>
+        <article>
+          <h2>my name is</h2>
+          <h1><span className='highlight closer_span'>&#91;</span> Marcel HESS <span className='highlight closer_span'>&#93;</span></h1>
+
+          <p className='basic_info about_para'>
+          I am self-taught junior front-end developer eager for first web developer job. <br /> <br />
+          Passionate about creating impactful websites.  <br /><br />
+          Seeking opportunities to learn and contribute.
+          </p>
+
+          <div className="divider"></div>
+
+          <p className='hobbys_info about_para'>
+          I love sports like running, working out, and swimming to keep myself energized. <br /><br />
+          Nature inspires me and brings me peace. In my spare time, I enjoy reading.
+          </p>
+        </article>
+
+
+      </section>
+      <div className="fill"></div>
+      <section id='s2' className='section-2'></section>
+      <div className="fill"></div>
+      <section id='s3' className='section-3'></section>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Links.tsx
@@ -0,0 +1,52 @@
+import { useState, useRef, useEffect } from 'react'
+
+function Links() {
+
+  const [activeLink, setActiveLink] = useState<string>("ABOUT")
+
+    
+  const anchorRef = useRef(null);
+
+  useEffect(() => {
+    // Simulate a click on the element on page load
+    if (anchorRef.current) {
+      anchorRef.current.click();
+    }
+  }, []);
+
+  function setLink(e: React.MouseEvent<HTMLAnchorElement>) {
+    const anchorElement = e?.currentTarget as HTMLAnchorElement
+    const anchorId = anchorElement.id
+    if (anchorId) {
+        setActiveLink(anchorId)
+    }
+  }
+
+  return (
+    <>
+      <ul className="section_selection">
+          <li className={activeLink === "ABOUT" ? "underline" : ""}>
+              <a ref={anchorRef} id='ABOUT' href="#s1" onClick={setLink}>ABOUT ME</a>
+          </li>
+
+          <li className={activeLink === "SKILLS" ? "underline" : ""}>
+              <a id='SKILLS' href="#s2" onClick={setLink}>SKILLS</a>
+          </li>
+
+          <li className={activeLink === "PROJECTS" ? "underline" : ""}>
+              <a id='PROJECTS' href="#s3" onClick={setLink}>PROJECTS</a>
+          </li>
+        </ul>
+        <div className="socials">
+          <a href="https://github.com/Maacaa0" target='_blank' className="socials_icon">
+            <img src="images/github.svg" alt="github icon" />
+          </a>
+          <a href="https://www.linkedin.com/in/marcel-hess-35a081257/" target='_blank' className="socials_icon">
+            <img src="images/linkedin.svg" alt="linkedin icon" />
+          </a>
+        </div>
+      </>
+  )
+}
+
+export default Links
