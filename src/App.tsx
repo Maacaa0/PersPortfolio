598e54cea2ec5d88b81de50d47138f8464e133e2
Maca <hesss.marcel@gmail.com>
2023-08-24 12:15:31 +0200

    first look at projects(mobile)

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,75 @@
 import { useState } from 'react';
 import Links from './components/Links'
+import Project from './components/Project';
 import './index.css'
+import projectData from "./projectData"
+
+
 
 function App() {
-
+  const [projectArr, setProjectArr] = useState(projectData)
+  
   const scrollToSection = (sectionId: string) => {
     const section = document.getElementById(sectionId);
     if (section) {
       section.scrollIntoView({ behavior: 'smooth' });
     }
   };
-
+  
   const [isCopied, setIsCopied] = useState<boolean>(false);
-
+  
   const copyToClipBoard = () => {
     const textToCopy = "hesss.marcel@gmail.com"
-
+    
     navigator.clipboard.writeText(textToCopy)
-
+    
     setIsCopied(true);
-
+    
     setTimeout(() => {
       setIsCopied(false);
     }, 2000); // Display the confirmation for 2 seconds
   }
 
+  const toggleProject = (id: number) => {
+    setProjectArr(prevState => {
+      return prevState.map(project => {
+        return project.id === id ? {...project, isShown: !project.isShown} : {...project, isShown: false}
+      })
+    }) 
+
+    const closeProject = () => {
+      console.log("id is:", id)
+    } 
+
+    window.addEventListener('click', closeProject)
+    closeProject()
+    window.removeEventListener('click', closeProject)
+  }
+  
+  const projects = projectArr.map(project => {
+    return (
+      <Project projectName={project.projectName}
+               projectInfo={project.projectInfo}
+               repo={project.repo}
+               liveSite={project.liveSite}
+               imgSrc={project.imgSrc}
+               isShown={project.isShown}
+               key={project.id}
+               id={project.id}
+               toggleProject={toggleProject}
+                />
+    )
+  })
+  
   return (
     <main>
       <Links scrollToSection={scrollToSection} />
       <section id='s1' className='section-1'>
         <article>
-          <h2>my name is</h2>
-          <h1>Marcel HESS</h1>
+          <div className="typewriter">
+            <h2>my name is</h2>
+            <h1>Marcel HESS</h1>
+          </div>
 
           <p className='basic_info about_para'>
           I am self-taught junior front-end developer eager for first web developer job. <br /> <br />
@@ -49,12 +87,19 @@ function App() {
 
 
       </section>
-      {/* <div className="fill"></div> */}
       <section id='s2' className='section-2'></section>
-      {/* <div className="fill"></div> */}
-      <section id='s3' className='section-3'></section>
+
+
+      <section id='s3' className='section-3'>
+
+        {projects}
+
+      </section>
+
+
+
       <div className="socials">
-        {isCopied && <div className='confirmation_box'>Email copied to Clipboard!</div>}
+        {isCopied && <div className='confirmation_box'>E-mail copied to Clipboard!</div>}
         <a className='socials_icon email_icon' onClick={copyToClipBoard}> <img src="images/email.svg" alt="email icon" />
         </a>
           <a href="https://github.com/Maacaa0" target='_blank' className="socials_icon">
