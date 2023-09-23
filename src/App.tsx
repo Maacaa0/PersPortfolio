8790cb9a6ceeafc056d799a312fa0c6faf6793b5
Maca <hesss.marcel@gmail.com>
2023-09-23 13:04:49 +0200

    changed width of wave on smaller devices

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,8 @@ import Navbar from './components/Navbar'
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
 import Skills from './components/Skills/Skills';
+import Footer from './components/Footer';
 import './index.css'
-
-import { ghIcon, linkedInIcon, fbIcon } from './components/Icons';
-
-
 
 function App() {
  
@@ -18,24 +15,8 @@ function App() {
       <About />
       <Skills />
       <Projects />
-
-      <footer>
-      <div className="footer_socials">
-
-        <a href="https://github.com/Maacaa0" target='_blank' className="footer_socials_icon">
-          {ghIcon}
-        </a>
-        <a href="https://www.linkedin.com/in/marcel-hess-35a081257/" target='_blank' className="footer_socials_icon">
-          {linkedInIcon}
-        </a>
-        <a href="https://www.facebook.com/hess.marcel/" target='_blank' className="footer_socials_icon">
-          {fbIcon}
-        </a>
-      </div>  
-      <a href="mailto:hesss.marcel@gmail.com">hesss.marcel@gmail.com</a>
-      <a href="tel:+420739254332">+420 739 254 332</a>
-      <small className='copyright'>&copy; 2023 Czech Republic Maca</small>
-      </footer>
+      <Footer />
+      
     </main>
   )
 }
