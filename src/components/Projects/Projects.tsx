5c17338f79c68f0520118335431bd21e12fb11fb
Maca <hesss.marcel@gmail.com>
2023-09-23 09:04:12 +0200

    projects redo

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,69 +1,135 @@
-import React, { useState } from 'react';
-import projectData from './projectData';
-
+import React, { useState } from "react";
+import projectData from "./projectData";
 
 const Projects: React.FC = () => {
+  const [projectArr, setProjectArr] = useState(projectData);
 
-  const [projectArr, setProjectArr] = useState(projectData)
-    
   const toggleProject = (id: number) => {
-    setProjectArr(prevState => {
-      return prevState.map(project => {
-        return project.id === id ? {...project, isShown: !project.isShown} : {...project, isShown: false}
-      })
-    }) 
-  }
+    setProjectArr((prevState) => {
+      return prevState.map((project) => {
+        return project.id === id
+          ? { ...project, isShown: !project.isShown }
+          : { ...project, isShown: false };
+      });
+    });
+  };
 
   const closeProject = () => {
-    setProjectArr(prevState => {
-      return prevState.map(project => {
-        return {...project, isShown: false}
-      })
-    })
+    setProjectArr((prevState) => {
+      return prevState.map((project) => {
+        return { ...project, isShown: false };
+      });
+    });
+  };
+
+  const projects = projectArr.map((project) => {
+    return (
+      <div
+        onClick={() => toggleProject(project.id)}
+        className={
+          project.id % 2 === 0
+            ? "project_container"
+            : "project_container reversed"
+        }
+        key={project.id}
+      >
+        <div className="project_image">
+          <img
+            className="project_img"
+            src={project.imgSrc}
+            alt="project image"
+          />
+        </div>
+        <div className="project_info">
+          <div>
+            <h3 className="project_name">{project.projectName}</h3>
+            <small className="project_info_text">{project.projectInfo}</small>
+          </div>
+          <small className="created_with">
+            Created with:
+            <br />
+            {project.buildWith}
+          </small>
+        </div>
+      </div>
+    );
+  });
+
+  {
+    /* --------CARD TO SHOW ON CLICK----------- */
   }
-  
-  const projects = projectArr.map(project => {
+  const projectCard = projectArr.map((project) => {
     return (
       <div key={project.id}>
-      <div onClick={()=>toggleProject(project.id)} className="projects">
-        <h3 className='project_name'>{project.projectName}</h3>
-        <img className='project_img' src={project.imgSrc} alt="project image" />
-        <div className="project_bottom_container">
-          <small className='project_info'>{project.projectInfo}</small>
+        {project.isShown && (
+          <div onClick={closeProject} className="overlay_page"></div>
+        )}
+        {project.isShown && (
+          <div
+            onClick={() => toggleProject(project.id)}
+            className="shown_project"
+          >
+            <h3 className="shown_project_name">{project.projectName}</h3>
+            <img
+              className="shown_project_img"
+              src={project.imgSrc}
+              alt="project image"
+            />
+            <div className="shown_project_bottom_container">
+              <small className="shown_project_info">
+                {project.projectInfo}
+                <br />
+                <br />
+                <small className="shown_created_with">
+                  Created with:
+                  <br />
+                  {project.buildWith}
+                </small>
+              </small>
 
-          
-        </div>
-        <div className="overlay"></div>
-      </div>
-
-        {/* --------CARD TO SHOW ON CLICK----------- */}
-      {project.isShown && <div onClick={closeProject} className='overlay_page'></div>}
-      {project.isShown && <div onClick={()=>toggleProject(project.id)} className="shown_projects">
-        <h3 className='project_name'>{project.projectName}</h3>
-        <img className='project_img' src={project.imgSrc} alt="project image" />
-        <div className="project_bottom_container">
-          <small className='project_info'>{project.projectInfo}</small>
-
-          <div className="project_links_container">
-             <a href={project.liveSite} target='_blank' className="project_live_site"><img className='link_ico' src="images/link.svg" alt="link icon" />preview site</a>
-             <a href={project.repo} target='_blank' className="project_repo"><img className='link_ico' src="images/ghlink.svg" alt="link icon" />repository</a>
+              <div className="project_links_container">
+                <a
+                  href={project.liveSite}
+                  target="_blank"
+                  className="project_live_site"
+                >
+                  <img
+                    className="link_ico"
+                    src="images/link.svg"
+                    alt="link icon"
+                  />
+                  preview site
+                </a>
+                <a href={project.repo} target="_blank" className="project_repo">
+                  <img
+                    className="link_ico"
+                    src="images/ghlink.svg"
+                    alt="link icon"
+                  />
+                  repository
+                </a>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>}
-
+        )}
       </div>
-  )})
+    );
+  });
 
   return (
-    <section className='section-2'>
+    <section className="section-2">
       <div className="projects_top_wrapper">
-        <p className="projects_text text">Here are some of my projects I coded to practice and harden my skills.</p>
-        <small className="highlightnt highlight">Click / Tap on project card to show more info</small>
+        <p className="projects_text text">
+          Here are some of my projects I coded to practice and harden my skills.
+        </p>
+        <small className="highlightnt highlight">
+          Click / Tap on project card to show more info
+        </small>
       </div>
-      <div className="projects_wrapper">
-        {projects}
-      </div>
-      </section>
-)};
+      <div className="projects_wrapper">{projects}</div>
+      {projectCard}
+    </section>
+  );
+};
 
 export default Projects;
--- src/components/Projects/projectData.tsx
+++ src/components/Projects/projectData.tsx
@@ -6,15 +6,17 @@ const projectData = [
       repo: "https://github.com/Maacaa0/workout_timer",
       liveSite: "https://maacaa0.github.io/workout_timer/",
       imgSrc: "/images/timer_.jpeg",
+      buildWith: "HTML / CSS / JAVASCRIPT",
       isShown: false
     },
     {
       id: 2,
       projectName: "Snake Game",
-      projectInfo: "An arcade game where you move a snake around and eat dots.",
+      projectInfo: "A classic arcade game where you move a snake around and eat food to grow longer.",
       repo: "https://github.com/Maacaa0/snake-game",
       liveSite: "https://maacaa0.github.io/snake-game/",
       imgSrc: "/images/snake_.jpeg",
+      buildWith: "HTML / CSS / JAVASCRIPT",
       isShown: false
     },
     {
@@ -24,15 +26,17 @@ const projectData = [
       repo: "https://github.com/Maacaa0/TODO-list",
       liveSite: "https://maacaa0.github.io/TODO-list/",
       imgSrc: "/images/todo_.jpeg",
+      buildWith: "HTML / CSS / JAVASCRIPT",
       isShown: false
     },
     {
       id: 4,
       projectName: "Tenzies",
-      projectInfo: "A simple child game where you roll the dice until you get Tenzies.",
+      projectInfo: "A game where objective is to roll the dice until all of them show the same number (Tenzies).",
       repo: "https://github.com/Maacaa0/Tenzies_React",
       liveSite: "https://shiny-cocada-1768e1.netlify.app/",
       imgSrc: "/images/tenzies_.jpeg",
+      buildWith: "CSS / REACT",
       isShown: false
     }
   ]
