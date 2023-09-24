f448cab9b4f7e17cf01ed0e5a934abf50d06f222
Maca <hesss.marcel@gmail.com>
2023-09-24 13:10:02 +0200

    minor fixes

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -42,7 +42,7 @@ const Skills = () => {
     <article className="skills_article">
             <div className="svg_wrapper">
                 { codeIcon }
-                <div className="text">I started learning front-end development in early 2022. I've been using <span className="highlight_red">YouTube</span> tutorials and <a target="_blank" href="https://www.freecodecamp.org/" className="highlight">FreeCodeCamp</a> to learn, and I'm also practicing my skills with challenges from <a target="_blank" href="https://www.frontendmentor.io/challenges" className="highlight">Frontend Mentor</a> and <a target="_blank" href="https://www.codewars.com/users/Maacaa0" className="highlight_red">Codewars</a>.
+                <div className="text">I started learning front-end development in early 2022. I've been using <span className="highlight_red">YouTube</span> tutorials and <a target="_blank" href="https://www.freecodecamp.org/" className="highlight">FreeCodeCamp</a> to learn, and I'm also practicing my skills with challenges from <a target="_blank" href="https://www.frontendmentor.io/challenges" className="highlight">Frontend Mentor</a> and <a target="_blank" href="https://www.codewars.com/users/Maacaa0" className='highlight'>Codewars</a>.
             <div className="icons_container">
                 {skills}
             </div>
@@ -52,8 +52,8 @@ const Skills = () => {
 
             <div className="svg_wrapper">
                 {lightBulbIcon}
-                <p className="text"> I have learned <span className="highlight">HTML</span>, <span className="highlight">CSS</span> and <span className="highlight">Javascript</span> essentials. Now I am on a path to learn <span className="highlight_lightBlue">ReactJs</span> with <span className="highlight_darkBlue">Typescript</span> and <span className="highlight_red">Git</span> to take my skills to the next level. <br /> <br />
-            I understand, there is so much I dont know yet, but I am excited to explore and learn.
+                <p className="text"> I have learned <span className="highlight">HTML</span>, <span className="highlight">CSS</span> and <span className="highlight">Javascript</span> essentials. Now I am on a path to learn React with Typescript and Git to take my skills to the next level. <br /> <br />
+                I understand, there is so much I dont know yet, but I am excited to explore and learn.
                 </p>
             </div>
            
