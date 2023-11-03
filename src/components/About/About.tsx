04ea14a77ff92496df4eee5d0c29cc51195caf9f
Maca <hesss.marcel@gmail.com>
2023-11-03 23:11:05 +0100

    firebase removed? minor changes

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -33,21 +33,7 @@ const About = ({ lang }: LangProps) => {
         </div>
       </article>
       <div className={styles.wave}>
-        {/* <svg
-          className={styles.firstWave}
-          viewBox="0 0 500 150"
-          preserveAspectRatio="none"
-        >
-          <path d="M-2.82,0.52 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z"></path>
-        </svg>
-
-        <svg
-          className={styles.secondWave}
-          viewBox="0 0 500 150"
-          preserveAspectRatio="none"
-        >
-          <path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z"></path>
-        </svg> */}
+        
       </div>
     </section>
   );
