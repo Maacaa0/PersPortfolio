e1c64b2f205b94c00aa8f8265b7252342ffb29f0
Maca <hesss.marcel@gmail.com>
2024-04-27 19:58:50 +0200

    fallback page

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,44 @@
-import Navbar from './components/Navbar/Navbar'
-import About from './components/About/About';
-import Projects from './components/Projects/Projects';
-import Skills from './components/Skills/Skills';
-import Footer from './components/Footer/Footer';
-import './index.css'
-import useLocalStorage from './hooks/useLocalStorage';
+// import Navbar from './components/Navbar/Navbar'
+// import About from './components/About/About';
+// import Projects from './components/Projects/Projects';
+// import Skills from './components/Skills/Skills';
+// import Footer from './components/Footer/Footer';
+// import './index.css'
+// import useLocalStorage from './hooks/useLocalStorage';
 
 export type LangProps = {
-  lang: string
-}
+  lang: string;
+};
 
 function App() {
-  const [lang, setLang] = useLocalStorage({
-    key: "language",
-    initialValue: "cz",
-  });
+  // const [lang, setLang] = useLocalStorage({
+  //   key: "language",
+  //   initialValue: "cz",
+  // });
 
-  function toggleLanguage() {
-    setLang(lang === "en" ? "cz" : "en");
-  }
- 
+  // function toggleLanguage() {
+  //   setLang(lang === "en" ? "cz" : "en");
+  // }
+
   return (
-    <main>
+    <main
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: 'center',
+      }}
+    >
       {/* br tag to avoid margin collapsing? I don't really get it  */}
-      <br /> 
-      <Navbar lang={lang} toggleLanguage={toggleLanguage} />
+      <br />
+      {/* <Navbar lang={lang} toggleLanguage={toggleLanguage} />
       <About lang={lang} />
       <Skills lang={lang} />
       <Projects lang={lang} />
-      <Footer />
-      
+      <Footer /> */}
+      <h1>HELLO WORLD</h1>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
